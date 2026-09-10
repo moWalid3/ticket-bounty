d88ff6ad310fc471743d39b2dae930d0a6ac9876
Mohamed <mohamedwalidtharwat2020@gmail.com>
2026-09-11 01:08:05 +0300

    create Heading component

--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -10,8 +10,8 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 
+import Heading from "@/components/Heading";
 import { buttonVariants } from "@/components/ui/button";
-import { Separator } from "@/components/ui/separator";
 import { LucideCircleCheck, LucideFileText, LucidePencil } from "lucide-react";
 
 const TICKET_ICONS = {
@@ -23,14 +23,7 @@ const TICKET_ICONS = {
 function TicketsPage() {
   return (
     <div className="flex-1 flex flex-col gap-y-8">
-      <div>
-        <h2 className="text-3xl font-bold tracking-tight">Tickets</h2>
-        <p className="text-sm text-muted-foreground">
-          All your tickets at one place
-        </p>
-      </div>
-
-      <Separator />
+      <Heading description="All your tickets at one place" title="Tickets" />
 
       <div className="flex-1 flex flex-col items-center gap-y-4 animate-fade-in-from-top">
         {initialTickets.map((ticket) => (
