eed5a379de78e1bcf8fc5f485331d776b0d6a729
Mohamed <mohamedwalidtharwat2020@gmail.com>
2026-09-12 00:51:39 +0300

    tiny improve for theme switcher logic

--- src/components/theme/theme-switcher.tsx
+++ src/components/theme/theme-switcher.tsx
@@ -4,20 +4,14 @@ import { LucideMoon, LucideSun } from "lucide-react";
 import { useTheme } from "next-themes";
 import { Button } from "../ui/button";
 
-function getToggledTheme(theme: string, systemTheme?: "dark" | "light") {
-  if (theme === "system") return systemTheme === "dark" ? "light" : "dark";
-
-  return theme === "dark" ? "light" : "dark";
-}
-
 function ThemeSwitcher() {
-  const { setTheme, systemTheme } = useTheme();
+  const { resolvedTheme, setTheme } = useTheme();
 
   return (
     <Button
       variant="outline"
       size="icon"
-      onClick={() => setTheme((theme) => getToggledTheme(theme, systemTheme))}
+      onClick={() => setTheme(resolvedTheme === "dark" ? "light" : "dark")}
     >
       <LucideSun className="size-4 rotate-0 scale-100 transition-all dark:rotate-90 dark:scale-0" />
       <LucideMoon className="absolute size-4 rotate-90 scale-0 transition-transform dark:rotate-0 dark:scale-100" />
