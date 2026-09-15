202d23ed90dcf694493baf5ff1f37ced9771e15f
Mohamed <mohamedwalidtharwat2020@gmail.com>
2026-09-15 23:43:24 +0300

    add tabs to profile and password page

--- src/components/heading.tsx
+++ src/components/heading.tsx
@@ -3,11 +3,13 @@ import { Separator } from "@/components/ui/separator";
 type HeadingProps = {
   title: string;
   description?: string;
+  tabs?: React.ReactNode;
 };
 
-function Heading({ title, description }: HeadingProps) {
+function Heading({ title, description, tabs }: HeadingProps) {
   return (
     <>
+      {tabs}
       <div className="px-2 sm:px-8">
         <h2 className="text-3xl font-bold tracking-tight">{title}</h2>
         {description && (
